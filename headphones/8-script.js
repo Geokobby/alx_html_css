e42434e46b03535aae17b5ce19acd78c360843e0
George Obeng-Boateng <geokobbymail@gmail.com>
2025-06-30 21:11:07 +0000

    8-script.js

---
+++ headphones/8-script.js
@@ -0,0 +1,7 @@
+const humbur = document.getElementById('humber');
+const navDisplay = document.getElementById('nav');
+
+humbur.addEventListener('click', drop)
+function drop() {
+    navDisplay.classList.toggle('hidden');
+ }
